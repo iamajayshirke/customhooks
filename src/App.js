75b26abd85a405faaffced48656cceb23b16584c
Ajay Shirke <ajay.shirke@fugetroncorp.com>
2022-07-07 22:04:36 +0530

    recaptach changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-
-import React, { useEffect, useState } from "react";
-import  Pages  from "./components/Pages";
+import ReCaptach from "./components/ReCaptach";
+import React from "react";
 
 function App() {
-  const [data, setData] = useState([]);
-  //getting posts
-  useEffect(() => {
-    const paginationFunc = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      const data = await res.json();
-      setData(data);
-      console.log(data);
-    };
-    paginationFunc();
-  }, []);
   return (
-    <div>
-    {data && data.length > 0 ? <Pages data={data} /> : <p>Loading...</p> }
-    </div>
+    <>
+      <ReCaptach />
+    </>
   );
 }
 
